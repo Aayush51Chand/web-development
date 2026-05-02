8823fcfcf7200836edd38f57e3694e8298b5c8d6
Aayush Chand <chandaayush51@gmail.com>
2026-05-02 19:33:43 +0545

    this is edited one

--- function.js
+++ function.js
@@ -10,15 +10,41 @@
 // let result = sum(a, b)
 // console.log(result)
 
-function loop()
+
+
+// function to add number from 1 to 10 using for loop in function with out passing arguments and parameter
+
+
+
+
+// function loop()
+// {
+//     let sum = 0;
+//     for(let x = 1; x <= 10; x++)
+//     {
+//       sum += x;
+//       console.log("the sum of the given number is given below:")  
+//     }
+//     return sum;
+// }
+// let a = loop()
+// console.log("the value is: "+a)
+
+
+// using function with for loop by passing arguments and parameter
+// to add numbers
+
+// let a = prompt("enter a number to find sum from");
+// let b = prompt("enter num up to find sum");
+let a = 1
+let b = 15
+function sum(x, y)
 {
-    let sum = 0;
-    for(let x = 1; x <= 10; x++)
+    let sum = 0
+for(let i = x; i <= y; i++)
     {
-      sum += x;
-      console.log("the sum of the given number is given below:")  
+sum += i;
     }
     return sum;
 }
-let a = loop()
-console.log("the value is: "+a)
+console.log(sum(a, b))
